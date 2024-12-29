ff3086785ae892c18c8c050a981162bfdb8d3515
StanLech <michalbaca@interia.pl>
2024-12-29 19:05:25 +0100

    Update streamlit_app.py
    rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,20 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-streamlit.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
-streamlit.write(
+st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
+st.write(
     """Choose the fruits you want in your custom Smoothie.
     """
 )
 
-name_an_order = streamlit.text_input("Name on Smoothie:")
-streamlit.write("The name on your Smoothie will be:", name_an_order)
-cnx = streamlit.connection("snowflake")
+name_an_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_an_order)
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#streamlit.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = streamlit.multiselect(
+ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections = 5
@@ -38,4 +38,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        streamlit.success('Your Smoothie is ordered, ' + name_an_order + '!', icon="✅")
+        st.success('Your Smoothie is ordered, ' + name_an_order + '!', icon="✅")
