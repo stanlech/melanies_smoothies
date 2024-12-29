91cfe56fc2c6be4835bf2c4ecb9414ebaa8b479b
StanLech <michalbaca@interia.pl>
2024-12-29 19:31:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_contrainer_width=True)
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
